61ab008988ed3747a6395b2a2409c6370f14a7b5
Miëtek Bak <mietek@bak.io>
2014-07-31 21:53:18 +0100

    WIP

--- src/cascrape.hs
+++ src/cascrape.hs
@@ -7,14 +7,13 @@
 module Main (main) where
 
 import Control.Monad.State (StateT, evalStateT, liftIO)
-import Control.Lens (assign, makeLenses, set, use, view)
+import Control.Lens ((.=), (^.), makeLenses, set, use)
 import Data.ByteString.Lazy.Char8 (ByteString)
 import Data.Maybe (isJust, listToMaybe)
-import Network.Wreq (FormParam)
+import Network.Wreq (FormParam ((:=)), responseBody, responseCookieJar)
 import System.Environment (getEnv)
 import Text.HTML.TagSoup ((~/=))
 
-import qualified Data.ByteString.Char8 as Sbs
 import qualified Data.ByteString.Lazy.Char8 as Lbs
 import qualified Network.HTTP.Client as H
 import qualified Network.Wreq as W
@@ -34,6 +33,8 @@ data SessionState = SessionState
 makeLenses ''SessionState
 
 type Session a = StateT SessionState IO a
+
+type Response = W.Response ByteString
 
 type Tag = S.Tag ByteString
 
@@ -92,33 +93,33 @@ login user code pass = do
     (_, tags1) <- postWithSession mainUrl []
     let (c1, c2, c3) = fetchParts code tags1 mkCode
     (_, tags2) <- postWithSession mainUrl
-      [ param "UserID" user
-      , param "pwd1"   c1
-      , param "pwd2"   c2
-      , param "pwd3"   c3
+      [ "UserID" := user
+      , "pwd1"   := c1
+      , "pwd2"   := c2
+      , "pwd3"   := c3
       ]
     let (p1, p2, p3) = fetchParts pass tags2 mkPass
     (_, tags3) <- postWithSession mainUrl
-      [ param "pwd1" p1
-      , param "pwd2" p2
-      , param "pwd3" p3
+      [ "pwd1" := p1
+      , "pwd2" := p2
+      , "pwd3" := p3
       ]
     tags4 <- if isJust (findTag tags3 menu)
-    then do
-      (_, tags') <- postWithSession mainUrl
-        [ param "Menu" "Continue"
-        ]
-      return tags'
-    else
-      return tags3
+      then do
+        (_, tags') <- postWithSession mainUrl
+          [ "Menu" := ("Continue" :: String)
+          ]
+        return tags'
+      else
+        return tags3
     let accOwn  = Lbs.unpack (fetchTagTextAfter tags4 3 own)
         accNum  = Lbs.unpack (fetchTagValue tags4 acc)
         accName = Lbs.unpack (fetchTagTextAfter tags4 8 acc)
         accBal  = Lbs.unpack (fetchTagTextAfter tags4 12 acc)
-    assign sessionAccountOwner   (Just accOwn)
-    assign sessionAccountNumber  (Just accNum)
-    assign sessionAccountName    (Just accName)
-    assign sessionAccountBalance (Just accBal)
+    sessionAccountOwner   .= Just accOwn
+    sessionAccountNumber  .= Just accNum
+    sessionAccountName    .= Just accName
+    sessionAccountBalance .= Just accBal
   where
     mkCode n = "<label for=ipos" ++ show n ++ " class=tran_confirm>"
     mkPass n = "<label for=pos"  ++ show n ++ " class=tran_confirm>"
@@ -129,7 +130,7 @@ login user code pass = do
 logout :: Session ()
 logout = do
     _ <- postWithSession mainUrl
-      [ param "Menu" "Log Out"
+      [ "Menu" := ("Log Out" :: String)
       ]
     return ()
 
@@ -137,59 +138,54 @@ downloadTransactions :: Session ByteString
 downloadTransactions = do
     Just accNum <- use sessionAccountNumber
     _ <- postWithSession mainUrl
-      [ param "Acc" accNum
+      [ "Acc" := accNum
       ]
     _ <- postWithSession mainUrl
-      [ param "Menu" "Download Transaction Details"
+      [ "Menu" := ("Download Transaction Details" :: String)
       ]
     _ <- postWithSession mainUrl
-      [ param "Menu"       "Accept"
-      , param "fDay"       "01"
-      , param "fMonth"     "07"
-      , param "fYear"      "2014"
-      , param "tDay"       "31"
-      , param "tMonth"     "07"
-      , param "tYear"      "2014"
-      , param "FileFormat" "EXCEL"
+      [ "Menu"       := ("Accept" :: String)
+      , "fDay"       := ("01" :: String)
+      , "fMonth"     := ("07" :: String)
+      , "fYear"      := ("2014" :: String)
+      , "tDay"       := ("31" :: String)
+      , "tMonth"     := ("07" :: String)
+      , "tYear"      := ("2014" :: String)
+      , "FileFormat" := ("EXCEL" :: String)
       ]
     (res, _) <- postWithSession mainUrl
-      [ param "Menu" "Download"
+      [ "Menu" := ("Download" :: String)
       ]
-    return (view W.responseBody res)
+    return (res^.responseBody)
 
 --------------------------------------------------------------------------------
 
-getWithSession :: String -> Session (W.Response ByteString, [Tag])
+getWithSession :: String -> Session (Response, [Tag])
 getWithSession url = do
     jar <- use sessionCookies
-    res <- liftIO (W.getWith (set W.cookies jar W.defaults) url)
-    assign sessionCookies (view W.responseCookieJar res)
+    let opts = set W.cookies jar W.defaults
+    res <- liftIO (W.getWith opts url)
+    sessionCookies .= res^.responseCookieJar
     let tags = parseTags res
     return (res, tags)
 
-postWithSession :: String -> [FormParam] -> Session (W.Response ByteString, [Tag])
+postWithSession :: String -> [FormParam] -> Session (Response, [Tag])
 postWithSession url params = do
-    jar <- use sessionCookies
-    mtran <- use sessionToken
-    let extraParam = case mtran of
-            Just tran -> [ param "Trxn" tran ]
-            Nothing   -> []
-    let params' = extraParam ++ params
-    res <- liftIO (W.postWith (set W.cookies jar W.defaults) url params')
-    assign sessionCookies (view W.responseCookieJar res)
+    jar  <- use sessionCookies
+    tran <- use sessionToken
+    let opts = set W.cookies jar W.defaults
+        tranParams = ("Trxn" := tran) : params
+    res <- liftIO (W.postWith opts url tranParams)
     let tags = parseTags res
-    assign sessionToken (Lbs.unpack `fmap` findTagValue tags transactionTag)
+    sessionCookies .= res^.responseCookieJar
+    sessionToken   .= Lbs.unpack `fmap` findTagValue tags transactionTag
     return (res, tags)
   where
     transactionTag = "<input type=hidden name=Trxn>"
 
-parseTags :: W.Response ByteString -> [Tag]
+parseTags :: Response -> [Tag]
 parseTags res =
-    S.parseTags (view W.responseBody res)
-
-param :: Sbs.ByteString -> String -> FormParam
-param name str =
-    name W.:= str
+    S.parseTags (res^.responseBody)
 
 --------------------------------------------------------------------------------
 
