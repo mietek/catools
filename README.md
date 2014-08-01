3557fb53c0d7b76e0e76103e479e22dbb725387e
Miëtek Bak <mietek@bak.io>
2014-08-01 20:15:22 +0100

    WIP

--- README.md
+++ README.md
@@ -3,25 +3,20 @@
 CA Tools
 ========
 
+TODO
+
 To build:
 
-    $ cabal sandbox init
-    $ cabal install --dependencies-only
-    $ cabal build
+    cabal sandbox init
+    cabal install --dependencies-only
+    cabal build
 
 To run:
 
-    $ export CA_USERNAME=...
-    $ export CA_ACCESS_CODE=...
-    $ export CA_PASSWORD=...
-    $ cabal run -- 2014-07-01 2014-07-31
-    Account number:  55555555
-    Account name:    EXAMPLE LTD
-    Account balance: 9999.99
-    31Jul2014,"Example reference","Example detail",0.00,9999.99
-    ...
-    01Jul2014,"Example reference","Example detail",0.00,9999.99
-
+    export CA_USER_NAME=...
+    export CA_SECRET_ACCESS_CODE=...
+    export CA_SECRET_PASSWORD=...
+    cabal run -- 2014-07-01 2014-07-31
 
 Meta
 ----
--- src/cascrape.hs
+++ src/cascrape.hs
@@ -1,110 +1,207 @@
 --------------------------------------------------------------------------------
 
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeSynonymInstances #-}
 
 module Main (main) where
 
-import Control.Applicative ((<$>))
+import Control.Applicative ((<$>), pure)
+import Control.Exception (SomeException, handle)
 import Control.Monad (when)
 import Control.Monad.State (StateT, evalStateT, liftIO)
 import Control.Lens ((&), (.=), (.~), (<~), (^.), makeLenses, use)
 import Data.ByteString.Lazy.Char8 (ByteString)
-import Data.Char (isDigit)
+import Data.Char (isDigit, isSpace)
+import Data.Csv (FromField, FromRecord, ToField, ToRecord, encode, parseField, toField)
+import Data.Csv.Streaming (HasHeader (NoHeader), Records (Nil, Cons), decode)
 import Data.Decimal (Decimal)
-import Data.Maybe (fromJust, isJust, listToMaybe)
+import Data.Maybe (isJust, listToMaybe)
 import Data.Time.Clock (UTCTime)
-import Data.Time.Format (formatTime, parseTime)
+import GHC.Generics (Generic)
 import Network.HTTP.Client (CookieJar, createCookieJar)
 import Network.Wreq (FormParam ((:=)))
 import System.Environment (getArgs, getEnv, getProgName)
+import System.Exit (exitFailure)
+import System.IO (hPutStr, hPutStrLn, stderr)
 import System.Locale (defaultTimeLocale)
 import Text.HTML.TagSoup ((~/=), fromAttrib, fromTagText, parseTags)
 
-import qualified Data.ByteString.Lazy.Char8 as Lbs
-import qualified Network.Wreq as Wreq
-import qualified Text.HTML.TagSoup as Soup
+import qualified Data.ByteString.Char8 as S
+import qualified Data.ByteString.Lazy.Char8 as L
+import qualified Data.Time.Format as T
+import qualified Network.Wreq as W
+import qualified Text.HTML.TagSoup as S
 
 --------------------------------------------------------------------------------
 
-type Tag = Soup.Tag ByteString
+newtype Amount = Amount Decimal
+  deriving (Eq, Num, Ord)
+
+newtype Date = Date UTCTime
+
+type Tag = S.Tag ByteString
 
 data SessionState = SessionState
-    { _cookieJar      :: !CookieJar
-    , _responseBody   :: !ByteString
-    , _responseTags   :: ![Tag]
-    , _responseToken  :: !ByteString
-    , _accountOwner   :: !ByteString
-    , _accountNumber  :: !ByteString
-    , _accountName    :: !ByteString
-    , _accountBalance :: !Decimal
+    { _userName         :: !String
+    , _secretAccessCode :: !String
+    , _secretPassword   :: !String
+    , _cookieJar        :: !CookieJar
+    , _responseBody     :: !ByteString
+    , _responseTags     :: ![Tag]
+    , _responseToken    :: !ByteString
+    , _accountOwner     :: !ByteString
+    , _accountNumber    :: !ByteString
+    , _accountName      :: !ByteString
+    , _currentBalance   :: !Amount
     }
 
 makeLenses ''SessionState
 
 type Session a = StateT SessionState IO a
 
+data Transaction = Transaction
+    { _transactionDate      :: !Date
+    , _transactionReference :: !ByteString
+    , _transactionDetails   :: !ByteString
+    , _transactionAmount    :: !Amount
+    , _transactionBalance   :: !Amount
+    }
+  deriving Generic
+
+makeLenses ''Transaction
+
 --------------------------------------------------------------------------------
 
 main :: IO ()
 main = do
-    getArgs >>= \case
-      [fromDate, toDate] -> do
-        user <- getEnv "CA_USERNAME"
-        code <- getEnv "CA_ACCESS_CODE"
-        pass <- getEnv "CA_PASSWORD"
-        flip evalStateT emptySession $ do
-          login user code pass
-          owner   <- use accountOwner
-          number  <- use accountNumber
-          name    <- use accountName
-          balance <- use accountBalance
-          csv     <- getTransactionsAsCsv fromDate toDate
-          liftIO $ do
-            putStrLn ("Account owner:   " ++ Lbs.unpack owner)
-            putStrLn ("Account number:  " ++ Lbs.unpack number)
-            putStrLn ("Account name:    " ++ Lbs.unpack name)
-            putStrLn ("Account balance: " ++ show balance)
-            Lbs.putStrLn csv
-          logout
-      _ -> do
-        name <- getProgName
-        putStrLn ("Usage: " ++ name ++ " YYYY-MM-DD YYYY-MM-DD")
+    (user, code, pass, from, to) <- getOpts
+    csv <- runInSession user code pass (getTransactionsAsCsv from to)
+    processTransactions (decode NoHeader csv)
+
+getOpts :: IO (String, String, String, Date, Date)
+getOpts =
+    handle showUsage $
+      getArgs >>= \case
+        [fromDate, toDate] -> do
+          user <- getEnv "CA_USER_NAME"
+          code <- getEnv "CA_SECRET_ACCESS_CODE"
+          pass <- getEnv "CA_SECRET_PASSWORD"
+          return (user, code, pass, read fromDate, read toDate)
+        _ ->
+          error "FROM_DATE TO_DATE: getArgs: unexpected format"
+  where
+    showUsage err = do
+      name <- getProgName
+      hPutStr stderr $ unlines
+        [ show (err :: SomeException)
+        , ""
+        , "Usage: " ++ name ++ " YYYY-MM-DD YYYY-MM-DD"
+        , ""
+        , "Environment variables:"
+        , "  CA_USER_NAME           Cater Allen user name"
+        , "  CA_SECRET_ACCESS_CODE  Cater Allen secret access code"
+        , "  CA_SECRET_PASSWORD     Cater Allen secret password"
+        ]
+      exitFailure
 
 --------------------------------------------------------------------------------
 
-baseUrl :: String
-baseUrl = "https://www.caterallenonline.co.uk"
+instance Read Amount where
+    readsPrec _ str =
+        let (dirty, rest) = break isSpace str
+            clean = filter (\c -> isDigit c || c == '-' || c == '.') dirty
+        in [(Amount (read clean), rest)]
 
-mainUrl :: String
-mainUrl = baseUrl ++ "/WebAccess.dll"
+instance Show Amount where
+    show (Amount amt) =
+        show amt
 
-login :: String -> String -> String -> Session ()
-login user code pass = do
-    _ <- liftIO (Wreq.get baseUrl)
-    postWithSession mainUrl []
-    (c1, c2, c3) <- prepareSecret codeChallenge code
-    postWithSession mainUrl
+instance FromField Amount where
+    parseField str =
+        pure (read (S.unpack str))
+
+instance ToField Amount where
+    toField amt =
+        S.pack (show amt)
+
+instance Read Date where
+    readsPrec _ str =
+        let (part, rest) = splitAt 10 str
+        in [(Date (readTime "%Y-%m-%d" part), rest)]
+
+instance Show Date where
+    show (Date date) =
+        formatTime "%Y-%m-%d" date
+
+instance FromField Date where
+    parseField str =
+        pure (Date (readTime "%d%b%Y" (S.unpack str)))
+
+instance ToField Date where
+    toField date =
+        S.pack (show date)
+
+instance FromRecord Transaction
+
+instance ToRecord Transaction
+
+processTransactions :: Records Transaction -> IO ()
+processTransactions (Cons (Right tr) more) = do
+    L.putStr (encode [tr])
+    processTransactions more
+processTransactions (Cons (Left err) more) = do
+    hPutStrLn stderr ("processTransactions: " ++ err)
+    processTransactions more
+processTransactions (Nil (Just err) rest) = do
+    hPutStrLn stderr ("processTransactions: " ++ err)
+    L.putStrLn rest
+processTransactions (Nil Nothing rest) =
+    L.putStrLn rest
+
+--------------------------------------------------------------------------------
+
+runInSession :: String -> String -> String -> Session a -> IO a
+runInSession user code pass act =
+    flip evalStateT (initState user code pass) $ do
+      login
+      res <- act
+      logout
+      return res
+
+login :: Session ()
+login = do
+    _ <- liftIO (W.get baseUrl)
+    access []
+    user <- use userName
+    code <- use secretAccessCode
+    (c1, c2, c3) <- scrapeSecret codeChallenge code
+    access
       [ "UserID" := user
       , "pwd1"   := c1
       , "pwd2"   := c2
       , "pwd3"   := c3
       ]
-    (p1, p2, p3) <- prepareSecret passChallenge pass
-    postWithSession mainUrl
+    pass <- use secretPassword
+    (p1, p2, p3) <- scrapeSecret passChallenge pass
+    access
       [ "pwd1" := p1
       , "pwd2" := p2
       , "pwd3" := p3
       ]
     whenM (isJust <$> tryScrapeTag continueTag) $
-      postWithSession mainUrl
+      access
         [ "Menu" := ("Continue" :: String)
         ]
     accountOwner   <~ scrapeTagTextAfter 3 ownerTag
     accountNumber  <~ fromAttrib "value" <$> scrapeTag accountTag
     accountName    <~ scrapeTagTextAfter 8 accountTag
-    accountBalance <~ acceptDecimal <$> scrapeTagTextAfter 12 accountTag
+    currentBalance <~ read . L.unpack <$> scrapeTagTextAfter 12 accountTag
   where
     continueTag     = "<input type=submit name=Menu value=Continue>"
     ownerTag        = "<div id=name_right_header_border"
@@ -114,77 +211,92 @@ login user code pass = do
 
 logout :: Session ()
 logout = do
-    postWithSession mainUrl
+    access
       [ "Menu" := ("Log Out" :: String)
       ]
 
-getTransactionsAsCsv :: String -> String -> Session ByteString
-getTransactionsAsCsv fromDate toDate = do
+getTransactionsAsCsv :: Date -> Date -> Session ByteString
+getTransactionsAsCsv (Date from) (Date to) = do
     accNum <- use accountNumber
-    postWithSession mainUrl
+    access
       [ "Acc" := accNum
       ]
-    postWithSession mainUrl
+    access
       [ "Menu" := ("Download Transaction Details" :: String)
       ]
-    postWithSession mainUrl
+    access
       [ "Menu"       := ("Accept" :: String)
-      , "fDay"       := fd
-      , "fMonth"     := fm
-      , "fYear"      := fy
-      , "tDay"       := td
-      , "tMonth"     := tm
-      , "tYear"      := ty
+      , "fDay"       := formatTime "%d" from
+      , "fMonth"     := formatTime "%m" from
+      , "fYear"      := formatTime "%Y" from
+      , "tDay"       := formatTime "%d" to
+      , "tMonth"     := formatTime "%m" to
+      , "tYear"      := formatTime "%Y" to
       , "FileFormat" := ("EXCEL" :: String)
       ]
-    postWithSession mainUrl
+    access
       [ "Menu" := ("Download" :: String)
       ]
     use responseBody
-  where
-    (fy, fm, fd) = splitIsoDate fromDate
-    (ty, tm, td) = splitIsoDate toDate
 
 --------------------------------------------------------------------------------
 
-emptySession :: SessionState
-emptySession =
+baseUrl :: String
+baseUrl =
+    "https://www.caterallenonline.co.uk"
+
+accessUrl :: String
+accessUrl =
+    baseUrl ++ "/WebAccess.dll"
+
+initState :: String -> String -> String -> SessionState
+initState user code pass =
     SessionState
-      { _cookieJar      = createCookieJar []
-      , _responseBody   = Lbs.empty
-      , _responseTags   = []
-      , _responseToken  = Lbs.empty
-      , _accountOwner   = Lbs.empty
-      , _accountNumber  = Lbs.empty
-      , _accountName    = Lbs.empty
-      , _accountBalance = 0
+      { _userName         = user
+      , _secretAccessCode = code
+      , _secretPassword   = pass
+      , _cookieJar        = createCookieJar []
+      , _responseBody     = L.empty
+      , _responseTags     = []
+      , _responseToken    = L.empty
+      , _accountOwner     = L.empty
+      , _accountNumber    = L.empty
+      , _accountName      = L.empty
+      , _currentBalance   = 0
       }
 
-postWithSession :: String -> [FormParam] -> Session ()
-postWithSession url params = do
+access :: [FormParam] -> Session ()
+access params = do
     jar   <- use cookieJar
     token <- use responseToken
-    let opts = Wreq.defaults & Wreq.cookies .~ jar
-    response <- liftIO (Wreq.postWith opts url (("Trxn" := token) : params))
-    cookieJar    .= response ^. Wreq.responseCookieJar
-    responseBody .= response ^. Wreq.responseBody
-    responseTags .= parseTags (response ^. Wreq.responseBody)
+    let opts      = W.defaults & W.cookies .~ jar
+        allParams = ("Trxn" := token) : params
+    response <- liftIO (W.postWith opts accessUrl allParams)
+    cookieJar    .= response ^. W.responseCookieJar
+    responseBody .= response ^. W.responseBody
+    responseTags .= parseTags (response ^. W.responseBody)
     tryScrapeTag tokenTag >>= \case
       Just tag -> responseToken .= fromAttrib "value" tag
-      Nothing  -> responseToken .= Lbs.empty
+      Nothing  -> responseToken .= L.empty
   where
     tokenTag = "<input type=hidden name=Trxn>"
 
-prepareSecret :: (Int -> String) -> String -> Session (String, String, String)
-prepareSecret challengeTag secret = do
+scrapeSecret :: (Int -> String) -> String -> Session (String, String, String)
+scrapeSecret challengeTag secret = do
     i1 <- readIndex <$> scrapeTagTextAfter 1 (challengeTag 1)
     i2 <- readIndex <$> scrapeTagTextAfter 1 (challengeTag 2)
     i3 <- readIndex <$> scrapeTagTextAfter 1 (challengeTag 3)
     return ([secret !! i1], [secret !! i2], [secret !! i3])
+  where
+    readIndex str = read (L.unpack str) - 1
+
+--------------------------------------------------------------------------------
 
 scrapeTag :: String -> Session Tag
 scrapeTag like =
-    fromJust <$> tryScrapeTag like
+    tryScrapeTag like >>= \case
+      Just tag -> return tag
+      Nothing  -> error (like ++ ": scrapeTag: not found")
 
 scrapeTagTextAfter :: Int -> String -> Session ByteString
 scrapeTagTextAfter n like =
@@ -192,7 +304,9 @@ scrapeTagTextAfter n like =
 
 scrapeTagAfter :: Int -> String -> Session Tag
 scrapeTagAfter n like =
-    fromJust <$> tryScrapeTagAfter n like
+    tryScrapeTagAfter n like >>= \case
+      Just tag -> return tag
+      Nothing  -> error (like ++ ": scrapeTagAfter: not found")
 
 tryScrapeTag :: String -> Session (Maybe Tag)
 tryScrapeTag like =
@@ -205,21 +319,15 @@ tryScrapeTagAfter n like = do
 
 --------------------------------------------------------------------------------
 
-splitIsoDate :: String -> (String, String, String)
-splitIsoDate s =
-    let date = fromJust (parse "%Y-%m-%d" s) :: UTCTime in
-    (format "%Y" date, format "%m" date, format "%d" date)
-  where
-    parse  = parseTime defaultTimeLocale
-    format = formatTime defaultTimeLocale
+readTime :: String -> String -> UTCTime
+readTime fmt str =
+    case T.parseTime defaultTimeLocale fmt str of
+      Just time -> time
+      Nothing   -> error (fmt ++ ": readTime: unexpected format")
 
-acceptDecimal :: ByteString -> Decimal
-acceptDecimal s =
-    read (filter (\c -> isDigit c || c == '.') (Lbs.unpack s))
-
-readIndex :: ByteString -> Int
-readIndex s =
-    read (Lbs.unpack s) - 1
+formatTime :: String -> UTCTime -> String
+formatTime fmt t =
+    T.formatTime defaultTimeLocale fmt t
 
 whenM :: (Monad m) => m Bool -> m () -> m ()
 whenM predAct act =
