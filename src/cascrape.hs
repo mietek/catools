65c929ad86acdfcc92d8b97eaa25afd10bf4a130
Miëtek Bak <mietek@bak.io>
2014-08-01 07:03:58 +0100

    WIP

--- src/cascrape.hs
+++ src/cascrape.hs
@@ -6,18 +6,24 @@
 
 module Main (main) where
 
+import Control.Applicative ((<$>))
 import Control.Monad (when)
 import Control.Monad.State (StateT, evalStateT, liftIO)
-import Control.Lens ((.=), (^.), makeLenses, set, use)
+import Control.Lens ((.=), (^.), (<~), makeLenses, set, use)
 import Data.ByteString.Lazy.Char8 (ByteString)
-import Data.Maybe (isJust, listToMaybe)
+import Data.Char (isDigit)
+import Data.Decimal (Decimal)
+import Data.Maybe (fromJust, isJust, listToMaybe)
+import Data.Time.Clock (UTCTime)
+import Data.Time.Format (formatTime, parseTime)
 import Network.HTTP.Client (CookieJar, createCookieJar)
-import Network.Wreq (FormParam ((:=)), cookies, defaults, getWith, postWith,
-  responseBody, responseCookieJar)
-import System.Environment (getEnv)
+import Network.Wreq (FormParam ((:=)))
+import System.Environment (getArgs, getEnv, getProgName)
+import System.Locale (defaultTimeLocale)
 import Text.HTML.TagSoup ((~/=), fromAttrib, fromTagText, parseTags)
 
 import qualified Data.ByteString.Lazy.Char8 as Lbs
+import qualified Network.Wreq as Wreq
 import qualified Text.HTML.TagSoup as Soup
 
 --------------------------------------------------------------------------------
@@ -25,14 +31,14 @@ import qualified Text.HTML.TagSoup as Soup
 type Tag = Soup.Tag ByteString
 
 data SessionState = SessionState
-    { _sessionCookies        :: CookieJar
-    , _sessionResponseBody   :: ByteString
-    , _sessionResponseTags   :: [Tag]
-    , _sessionToken          :: ByteString
-    , _sessionAccountOwner   :: ByteString
-    , _sessionAccountNumber  :: ByteString
-    , _sessionAccountName    :: ByteString
-    , _sessionAccountBalance :: ByteString
+    { _cookieJar      :: !CookieJar
+    , _responseBody   :: !ByteString
+    , _responseTags   :: ![Tag]
+    , _responseToken  :: !ByteString
+    , _accountOwner   :: !ByteString
+    , _accountNumber  :: !ByteString
+    , _accountName    :: !ByteString
+    , _accountBalance :: !Decimal
     }
 
 makeLenses ''SessionState
@@ -43,29 +49,28 @@ type Session a = StateT SessionState IO a
 
 main :: IO ()
 main = do
-    user <- getEnv "CA_USERNAME"
-    code <- getEnv "CA_ACCESS_CODE"
-    pass <- getEnv "CA_PASSWORD"
-    flip evalStateT emptySession $ do
-      login user code pass
-      accOwn  <- use sessionAccountOwner
-      accNum  <- use sessionAccountNumber
-      accName <- use sessionAccountName
-      accBal  <- use sessionAccountBalance
-      liftIO $ do
-        putMark
-        Lbs.putStrLn (Lbs.append "Account owner:    " accOwn)
-        Lbs.putStrLn (Lbs.append "Account number:   " accNum)
-        Lbs.putStrLn (Lbs.append "Account name:     " accName)
-        Lbs.putStrLn (Lbs.append "Account balance:  " accBal)
-        putMark
-      res <- downloadTransactions
-      liftIO $ do
-        Lbs.putStr res
-        putMark
-      logout
-  where
-    putMark = putStrLn (replicate 80 '-')
+    getArgs >>= \case
+      [fromDate, toDate] -> do
+        user <- getEnv "CA_USERNAME"
+        code <- getEnv "CA_ACCESS_CODE"
+        pass <- getEnv "CA_PASSWORD"
+        flip evalStateT emptySession $ do
+          login user code pass
+          owner   <- use accountOwner
+          number  <- use accountNumber
+          name    <- use accountName
+          balance <- use accountBalance
+          csv     <- getTransactionsAsCsv fromDate toDate
+          liftIO $ do
+            putStrLn ("Account owner:   " ++ Lbs.unpack owner)
+            putStrLn ("Account number:  " ++ Lbs.unpack number)
+            putStrLn ("Account name:    " ++ Lbs.unpack name)
+            putStrLn ("Account balance: " ++ show balance)
+            Lbs.putStrLn csv
+          logout
+      _ -> do
+        name <- getProgName
+        putStrLn ("Usage: " ++ name ++ " YYYY-MM-DD YYYY-MM-DD")
 
 --------------------------------------------------------------------------------
 
@@ -77,43 +82,35 @@ mainUrl = baseUrl ++ "/WebAccess.dll"
 
 login :: String -> String -> String -> Session ()
 login user code pass = do
-    getWithSession baseUrl
+    _ <- liftIO (Wreq.get baseUrl)
     postWithSession mainUrl []
-    tags1 <- use sessionResponseTags
-    let (c1, c2, c3) = fetchParts code tags1 mkCodeTag
+    (c1, c2, c3) <- prepareSecret codeChallenge code
     postWithSession mainUrl
       [ "UserID" := user
       , "pwd1"   := c1
       , "pwd2"   := c2
       , "pwd3"   := c3
       ]
-    tags2 <- use sessionResponseTags
-    let (p1, p2, p3) = fetchParts pass tags2 mkPassTag
+    (p1, p2, p3) <- prepareSecret passChallenge pass
     postWithSession mainUrl
       [ "pwd1" := p1
       , "pwd2" := p2
       , "pwd3" := p3
       ]
-    tags3 <- use sessionResponseTags
-    when (isJust (findTag tags3 conTag)) $
+    whenM (isJust <$> tryScrapeTag continueTag) $
       postWithSession mainUrl
         [ "Menu" := ("Continue" :: String)
         ]
-    tags4 <- use sessionResponseTags
-    let accOwn  = fetchTagTextAfter tags4 3 ownTag
-        accNum  = fetchTagValue tags4 accTag
-        accName = fetchTagTextAfter tags4 8 accTag
-        accBal  = fetchTagTextAfter tags4 12 accTag
-    sessionAccountOwner   .= accOwn
-    sessionAccountNumber  .= accNum
-    sessionAccountName    .= accName
-    sessionAccountBalance .= accBal
+    accountOwner   <~ scrapeTagTextAfter 3 ownerTag
+    accountNumber  <~ fromAttrib "value" <$> scrapeTag accountTag
+    accountName    <~ scrapeTagTextAfter 8 accountTag
+    accountBalance <~ acceptDecimal <$> scrapeTagTextAfter 12 accountTag
   where
-    conTag = "<input type=submit name=Menu value=Continue>"
-    ownTag = "<div id=name_right_header_border"
-    accTag = "<input type=submit name=Acc>"
-    mkCodeTag n = "<label for=ipos" ++ show n ++ ">"
-    mkPassTag n = "<label for=pos"  ++ show n ++ ">"
+    continueTag     = "<input type=submit name=Menu value=Continue>"
+    ownerTag        = "<div id=name_right_header_border"
+    accountTag      = "<input type=submit name=Acc>"
+    codeChallenge n = "<label for=ipos" ++ show n ++ ">"
+    passChallenge n = "<label for=pos"  ++ show n ++ ">"
 
 logout :: Session ()
 logout = do
@@ -121,9 +118,9 @@ logout = do
       [ "Menu" := ("Log Out" :: String)
       ]
 
-downloadTransactions :: Session ByteString
-downloadTransactions = do
-    accNum <- use sessionAccountNumber
+getTransactionsAsCsv :: String -> String -> Session ByteString
+getTransactionsAsCsv fromDate toDate = do
+    accNum <- use accountNumber
     postWithSession mainUrl
       [ "Acc" := accNum
       ]
@@ -132,94 +129,100 @@ downloadTransactions = do
       ]
     postWithSession mainUrl
       [ "Menu"       := ("Accept" :: String)
-      , "fDay"       := ("01" :: String)
-      , "fMonth"     := ("07" :: String)
-      , "fYear"      := ("2014" :: String)
-      , "tDay"       := ("31" :: String)
-      , "tMonth"     := ("07" :: String)
-      , "tYear"      := ("2014" :: String)
+      , "fDay"       := fd
+      , "fMonth"     := fm
+      , "fYear"      := fy
+      , "tDay"       := td
+      , "tMonth"     := tm
+      , "tYear"      := ty
       , "FileFormat" := ("EXCEL" :: String)
       ]
     postWithSession mainUrl
       [ "Menu" := ("Download" :: String)
       ]
-    use sessionResponseBody
+    use responseBody
+  where
+    (fy, fm, fd) = splitIsoDate fromDate
+    (ty, tm, td) = splitIsoDate toDate
 
 --------------------------------------------------------------------------------
 
 emptySession :: SessionState
 emptySession =
     SessionState
-      { _sessionCookies        = createCookieJar []
-      , _sessionResponseBody   = Lbs.empty
-      , _sessionResponseTags   = []
-      , _sessionToken          = Lbs.empty
-      , _sessionAccountOwner   = Lbs.empty
-      , _sessionAccountNumber  = Lbs.empty
-      , _sessionAccountName    = Lbs.empty
-      , _sessionAccountBalance = Lbs.empty
+      { _cookieJar = createCookieJar []
+      , _responseBody   = Lbs.empty
+      , _responseTags   = []
+      , _responseToken  = Lbs.empty
+      , _accountOwner   = Lbs.empty
+      , _accountNumber  = Lbs.empty
+      , _accountName    = Lbs.empty
+      , _accountBalance = 0
       }
-
-getWithSession :: String -> Session ()
-getWithSession url = do
-    jar <- use sessionCookies
-    let opts = set cookies jar defaults
-    res <- liftIO (getWith opts url)
-    let body = res ^. responseBody
-        tags = parseTags body
-    sessionCookies      .= res ^. responseCookieJar
-    sessionResponseBody .= body
-    sessionResponseTags .= tags
-    sessionToken        .= findTagValue tags tokTag
-  where
-    tokTag = "<input type=hidden name=Trxn>"
 
 postWithSession :: String -> [FormParam] -> Session ()
 postWithSession url params = do
-    jar <- use sessionCookies
-    tok <- use sessionToken
-    let opts = set cookies jar defaults
-    res <- liftIO (postWith opts url (("Trxn" := tok) : params))
-    let body = res ^. responseBody
-        tags = parseTags body
-    sessionCookies      .= res ^. responseCookieJar
-    sessionResponseBody .= body
-    sessionResponseTags .= tags
-    sessionToken        .= findTagValue tags tokTag
+    jar   <- use cookieJar
+    token <- use responseToken
+    let opts = set Wreq.cookies jar Wreq.defaults
+    response <- liftIO (Wreq.postWith opts url (("Trxn" := token) : params))
+    cookieJar    .= response ^. Wreq.responseCookieJar
+    responseBody .= response ^. Wreq.responseBody
+    responseTags .= parseTags (response ^. Wreq.responseBody)
+    tryScrapeTag tokenTag >>= \case
+      Just tag -> responseToken .= fromAttrib "value" tag
+      Nothing  -> responseToken .= Lbs.empty
   where
-    tokTag = "<input type=hidden name=Trxn>"
+    tokenTag = "<input type=hidden name=Trxn>"
+
+prepareSecret :: (Int -> String) -> String -> Session (String, String, String)
+prepareSecret challengeTag secret = do
+    i1 <- readIndex <$> scrapeTagTextAfter 1 (challengeTag 1)
+    i2 <- readIndex <$> scrapeTagTextAfter 1 (challengeTag 2)
+    i3 <- readIndex <$> scrapeTagTextAfter 1 (challengeTag 3)
+    return ([secret !! i1], [secret !! i2], [secret !! i3])
+
+scrapeTag :: String -> Session Tag
+scrapeTag like =
+    fromJust <$> tryScrapeTag like
+
+scrapeTagTextAfter :: Int -> String -> Session ByteString
+scrapeTagTextAfter n like =
+    fromTagText <$> scrapeTagAfter n like
+
+scrapeTagAfter :: Int -> String -> Session Tag
+scrapeTagAfter n like =
+    fromJust <$> tryScrapeTagAfter n like
+
+tryScrapeTag :: String -> Session (Maybe Tag)
+tryScrapeTag like =
+    tryScrapeTagAfter 0 like
+
+tryScrapeTagAfter :: Int -> String -> Session (Maybe Tag)
+tryScrapeTagAfter n like = do
+    tags <- use responseTags
+    return (listToMaybe (drop n (dropWhile (~/= like) tags)))
 
 --------------------------------------------------------------------------------
 
-findTag :: [Tag] -> String -> Maybe Tag
-findTag tags tag =
-    listToMaybe (dropWhile (~/= tag) tags)
-
-findTagValue :: [Tag] -> String -> ByteString
-findTagValue tags tag =
-    maybe Lbs.empty (fromAttrib "value") (findTag tags tag)
-
-fetchTag :: [Tag] -> String -> Tag
-fetchTag tags tag =
-    head (dropWhile (~/= tag) tags)
-
-fetchTagValue :: [Tag] -> String -> ByteString
-fetchTagValue tags tag =
-    fromAttrib "value" (fetchTag tags tag)
-
-fetchTagAfter :: [Tag] -> Int -> String -> Tag
-fetchTagAfter tags n tag =
-    head (drop n (dropWhile (~/= tag) tags))
-
-fetchTagTextAfter :: [Tag] -> Int -> String -> ByteString
-fetchTagTextAfter tags n tag =
-    fromTagText (fetchTagAfter tags n tag)
-
-fetchParts :: String -> [Tag] -> (Int -> String) -> (String, String, String)
-fetchParts whole tags mkTag =
-    (wrap (fetch 1), wrap (fetch 2), wrap (fetch 3))
+splitIsoDate :: String -> (String, String, String)
+splitIsoDate s =
+    let date = fromJust (parse "%Y-%m-%d" s) :: UTCTime in
+    (format "%Y" date, format "%m" date, format "%d" date)
   where
-    wrap s  = [whole !! (read s - 1)]
-    fetch n = Lbs.unpack (fetchTagTextAfter tags 1 (mkTag n))
+    parse  = parseTime defaultTimeLocale
+    format = formatTime defaultTimeLocale
+
+acceptDecimal :: ByteString -> Decimal
+acceptDecimal s =
+    read (filter (\c -> isDigit c || c == '.') (Lbs.unpack s))
+
+readIndex :: ByteString -> Int
+readIndex s =
+    read (Lbs.unpack s) - 1
+
+whenM :: (Monad m) => m Bool -> m () -> m ()
+whenM predAct act =
+    predAct >>= flip when act
 
 --------------------------------------------------------------------------------
